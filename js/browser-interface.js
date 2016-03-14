2c7b2b78bb210497eb181ad468f80c356f6e2bc3
Joe Karasek and Mary Warrington <joseph.karasek@gmail.com>
2016-03-14 10:55:01 -0700

    turn journal into its own object

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,15 +1,17 @@
-var journalPost = require('./../js/journal.js').journalPost;
+// var moment = require('moment');
+var journal = require('./../js/Journal.js').Journal;
 
 $(document).ready(function(){
   $('#journal-form').submit(function(event){
     event.preventDefault();
     var title = $('#title').val();
     var author = $('#author').val();
-    var body = $('#body').val();
+    var entry = $('#body').val();
+    var newJournalEntry = new journal(title, author, entry);
 
-    $('#posts').append("<li>" + title + "</li>");
-    $('#posts').append("<li>" + author + "</li>");
-    $('#posts').append("<li>" + body + "</li>");
+    $('#posts').append("<li>" + newJournalEntry.title + "</li>");
+    $('#posts').append("<li>" + newJournalEntry.author + "</li>");
+    $('#posts').append("<li>" + newJournalEntry.entry + "</li>");
 
   });
 });
